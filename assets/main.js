d063baa9e9747f03e0a5b2f87d13f1e4eb20b087
nelemon <104180185+nelemon@users.noreply.github.com>
2024-10-27 22:20:54 +1000

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,11 @@
 const animationElement = document.getElementById('animation');
+let currentRotation = 0; // Начальный угол поворота
 
 // Функция для запуска бесконечной анимации
 function startAnimation() {
     setInterval(() => {
-        // Добавляем класс для поворота
-        animationElement.classList.add('rotate');
-
-        // Удаляем класс после 0.2 секунды
-        setTimeout(() => {
-            animationElement.classList.remove('rotate');
-        }, 200); // 200 миллисекунд
+        currentRotation += 360; // Увеличиваем угол поворота на 360 градусов
+        animationElement.style.transform = `rotate(${currentRotation}deg)`; // Применяем поворот
     }, 1000); // Каждую 1 секунду
 }
 
