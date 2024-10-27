d187b81da68956f9d33670e0f2b2692a13676b22
nelemon <104180185+nelemon@users.noreply.github.com>
2024-10-27 23:52:56 +1000

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -6,7 +6,6 @@ function startAnimation() {
     setInterval(() => {
         currentRotation += 180; // Увеличиваем угол поворота на 180 градусов
         animationElement.style.transform = `rotate(${currentRotation}deg)`; // Применяем поворот
-    }, 1100); // Каждую 1 секунду
     }, 1200); // Каждую 1 секунду
 }
 
