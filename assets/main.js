e81438ddf6887a30f507f21d61db546d53fe70c2
nelemon <104180185+nelemon@users.noreply.github.com>
2024-10-27 22:00:27 +1000

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -3,9 +3,7 @@ const animationElement = document.getElementById('animation');
 // Функция для запуска анимации
 function startAnimation() {
     // Анимация длится 2 секунды
-    animationElement.style.transition = 'none'; // Отключаем плавный переход перед запуском
     setTimeout(() => {
-        animationElement.style.transition = 'transform 0.2s'; // Включаем плавный переход
         animationElement.classList.add('rotate'); // Добавляем класс для поворота
     }, 1000); // Через 1 секунду
 }
