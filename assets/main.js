737ca611c13a733d8f17dfda38a8266f52e2e074
nelemon <104180185+nelemon@users.noreply.github.com>
2024-10-28 13:20:13 +1000

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,22 @@
 const animationElement = document.getElementById('animation');
 let currentRotation = 0; // Начальный угол поворота
+let isRotating = false; // Флаг для отслеживания состояния поворота
 
-// Функция для запуска бесконечной анимации
-function startAnimation() {
-    setInterval(() => {
+// Функция для выполнения одного поворота
+function rotateAnimation() {
+    if (!isRotating) {
+        isRotating = true; // Устанавливаем флаг, что идет поворот
         currentRotation += 180; // Увеличиваем угол поворота на 180 градусов
+        animationElement.style.transition = 'transform 0.5s'; // Устанавливаем время поворота (0.5 секунды)
         animationElement.style.transform = `rotate(${currentRotation}deg)`; // Применяем поворот
-    }, 1000); // Каждую 1 секунду
+
+        // Запускаем таймер, чтобы подождать окончания поворота
+        setTimeout(() => {
+            isRotating = false; // Поворот завершен
+            rotateAnimation(); // Запускаем следующий поворот
+        }, 1000); // Ожидаем 1 секунду до следующего поворота
+    }
 }
 
 // Запускаем анимацию
-startAnimation();
+rotateAnimation();
